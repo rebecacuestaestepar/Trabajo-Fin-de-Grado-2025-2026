a529deddcf621d8d9a0fda008d7c1749f85364a6
Rebeca Cuesta Estepar <rebecacuestaestepar@gmail.com>
2026-05-05 13:54:14 +0200

    Hacer prototipo de función que carga las clases extraídas con el excel en base de datos

---
+++ app_horarios/reservas/services_excel.py
@@ -0,0 +1,50 @@
+from limpiar_nombres_aulas import limpiar_nombre_aula
+from excel_parser import DIAS_SEMANA
+from aulas.models import Aula
+from reservas.models import Reserva, ReservaPeriodica
+from docencia.models import Grupo
+from django.db import transaction
+
+import uuid
+
+def generar_reservas_periodicas(clases, fecha_inicio_sem, fecha_fin_sem):
+
+    with transaction.atomic():
+
+        num_reservas_creadas = 0
+
+        for clase in clases:
+
+            nombre_aula_limpio = limpiar_nombre_aula(clase.aula)
+
+            aula_objeto = Aula.objects.filter(nombre=nombre_aula_limpio).first()
+
+            if not aula_objeto:
+                continue
+
+            grupo_objeto = Grupo.objects.filter(id_asignatura=clase.cod_asig, nombre = clase.grupo).first()
+
+            if not grupo_objeto:
+                continue
+
+            dia_num = DIAS_SEMANA.get(clase.dia, 1)
+
+            reserva = Reserva.objects.create(
+                id_aula=aula_objeto,
+                id_dia=dia_num,
+                estado='A',
+                tipo='R',
+                hora_inicio=clase.hora_inicio,
+                hora_fin=clase.hora_fin
+            )
+
+            ReservaPeriodica.objects.create(
+                id_reserva=reserva,
+                id_grupo=grupo_objeto,
+                dia_semana=dia_num,
+                fecha_inicio=fecha_inicio_sem,
+                fecha_fin=fecha_fin_sem,
+                intervalo_semanas=1
+            )
+            num_reservas_creadas += 1
+            
