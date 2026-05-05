234fd534fd7010517a93b2eec7a9e4ea8f59fc3d
Rebeca Cuesta Estepar <rebecacuestaestepar@gmail.com>
2026-05-05 13:27:34 +0200

    Función que limpia los aulas delexcel, cuando encuentra Aula lo elimina a no ser de que sea Aula Empresa. Cuando encuentra Lab. o Lab lo sustituye por Laboratorio a no ser de que esté seguido por ejemplo de 53-A1 que en ese caso lo elimina

---
+++ app_horarios/reservas/limpiar_nombres_aulas.py
@@ -0,0 +1,23 @@
+import re
+
+def limpiar_nombre_aula(nombre_aula):
+    """
+    Limpiamos el nombre del aula quitando Aula, y convirtiendo lab. o lab en Laboratorio sino va seguido de aula.
+    """
+
+    if not nombre_aula:
+        return ""
+    
+    aula = str(nombre_aula).strip()
+
+    aula = re.sub(r'(?i)^Aula\s+(?!Empresa)', '', aula)
+    aula = re.sub(r'\s*\(.*?\)', '', aula).strip()
+
+    aula = re.sub(r'(?i)^Inf\.\s+', 'Informática ', aula)
+
+    aula = re.sub(r'(?i)^Lab\.?\s+(?=\d{2}-[a-zA-Z]\d)', '', aula)
+
+    aula = re.sub(r'(?i)^Lab\.?\s+', 'Laboratorio ', aula)
+
+    return aula.strip()
+    
