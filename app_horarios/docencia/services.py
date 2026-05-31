1b1c0f0efc0e9398b94460f61972e5a71cd1aae4
Rebeca Cuesta Estepar <rebecacuestaestepar@gmail.com>
2026-05-31 18:08:49 +0200

    Implementación servicios básicos para los docentes

--- app_horarios/docencia/services.py
+++ app_horarios/docencia/services.py
@@ -1,4 +1,4 @@
-from .models import Grado, Asignaturas, Grupo
+from .models import Grado, Asignaturas, Grupo, Docente
 from django.shortcuts import get_object_or_404
 
 class GradoService:
@@ -78,3 +78,29 @@ class GrupoService:
     def eliminar_grupo(grupo_id):
         grupo = GrupoService.obtener_grupo_por_id(grupo_id)
         grupo.delete()
+
+class DocenteService:
+    @staticmethod
+    def listar_docentes():
+        return Docente.objects.all().order_by('iddocente')
+
+    @staticmethod
+    def obtener_docente_por_id(docente_id):
+        return get_object_or_404(Docente, iddocente=docente_id)
+
+    @staticmethod
+    def crear_docente(datos_docente):
+        return Docente.objects.create(**datos_docente)
+
+    @staticmethod
+    def actualizar_docente(docente_id, datos_actualizados):
+        docente = get_object_or_404(Docente, iddocente=docente_id)
+        for campo, valor in datos_actualizados.items():
+            setattr(docente, campo, valor)
+        docente.save()
+        return docente
+
+    @staticmethod
+    def eliminar_docente(docente_id):
+        docente = DocenteService.obtener_docente_por_id(docente_id)
+        docente.delete()
