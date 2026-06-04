5441684bdbfba39f4f8b60e3c5e23f3046f00baf
Rebeca Cuesta Estepar <rebecacuestaestepar@gmail.com>
2026-06-04 13:56:58 +0200

    Introducir la url para endpoint que tiene Django para conseguir el token de refresh

--- app_horarios/usuarios/urls.py
+++ app_horarios/usuarios/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ListaMiniPermisosAPIView, ListaMiniRolesAPIView, LoginUniversidadAPIView, RolViewSet, UsuarioViewSet
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 routerUsuario = DefaultRouter()
 routerUsuario.register(r'usuarios', UsuarioViewSet, basename='usuario')
 routerRol = DefaultRouter()
@@ -15,4 +17,5 @@ urlpatterns = [
     path('', include(routerRol.urls)),
     path('mini-roles/', ListaMiniRolesAPIView.as_view(), name='lista_mini_roles'),
     path('mini-permisos/', ListaMiniPermisosAPIView.as_view(), name='lista_mini_permisos'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
