a2c0dca7c6c0b25578d5bb29e73abe4d424aba79
Rebeca Cuesta Estepar <rebecacuestaestepar@gmail.com>
2026-06-05 21:40:19 +0200

    Comprobaciones para ver por qué falla el método cors

--- app_horarios/nucleo/settings.py
+++ app_horarios/nucleo/settings.py
@@ -37,9 +37,27 @@ ALLOWED_HOSTS = [
     "https://django-production-876a.up.railway.app",
     "localhost",
 ]
-CORS_ALLOWED_ORIGINS = [
-    "https://front-react-production-25aa.up.railway.app",
-    "http://localhost:5173",
+# CORS_ALLOWED_ORIGINS = [
+#     "https://front-react-production-25aa.up.railway.app",
+#     "http://localhost:5173",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 
